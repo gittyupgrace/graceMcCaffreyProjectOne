08f3756e728bda9dad956b447f8087ada8613b6c
Grace <gracecmccaffrey@gmail.com>
2022-10-17 16:44:24 -0400

    refactored js and added namespacing for pop modal

--- scripts/homeScript.js
+++ scripts/homeScript.js
@@ -1,52 +1,38 @@
-//BLOG/NEWSLETTER SUBSCRIPTION POP UP
+const modalApp = {};
 
-//INITIAL PSEUDOCODE:
+modalApp.modalOpen = document.querySelector('.overlay')
+modalApp.closeModalButton = document.getElementById('closeModalButton');
+modalApp.modalForm = document.querySelector('.modalForm');
 
-//user comes to page and popup is there
-//user has to either input email or hit the x to make the popup disappear 
-//include something to ensure if user is inputing an email that it is in the correct format for an email
-
-//grab the things:
-
-const modalOpen = document.querySelector('.overlay')
-// console.log(modalOpen)
-
-const closeModalButton = document.getElementById('close-modal-button');
-// console.log(closeModalButton)
-
-const modalForm = document.querySelector('.modalForm');
-//console.log(modalForm)
-
-//function to open modal a couple seconds after page load
-function openModal() {
-    modalOpen.classList.add('modal-open');
+modalApp.openModal = () => {
+    modalApp.modalOpen.classList.add('modalOpen');
 }
 
-//function to close modal
-function closeModal() {
-    modalOpen.classList.remove('modal-open');
-    modalOpen.classList.add('modal-close');
+modalApp.closeModal = () => {
+    modalApp.modalOpen.classList.remove('modalOpen');
+    modalApp.modalOpen.classList.add('modalClose');
 }
 
-//user arrives at page, modal pops up after 2 seconds (class of modal-open added to overlay)
-setTimeout(openModal(), 2000);
+modalApp.clickClose = () => {
+    modalApp.closeModalButton.addEventListener('click', modalApp.closeModal);
+}
 
+modalApp.enterEmail = () => {
+    modalApp.modalForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        modalApp.closeModal();
+        modalApp.modalForm.reset();
+    });
+}
 
-//user clicks close-modal-button, add class modal-close to overlay
-closeModalButton.addEventListener('click', function () {
-    closeModal();
-})
+modalApp.loadModal = () => {
+    setTimeout(modalApp.openModal(), 2000);
+    modalApp.clickClose();
+    modalApp.enterEmail();
+}
 
-//user inputs email address and hits submit to close modal
-modalForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    closeModal();
-    modalForm.reset();
-});
+modalApp.init = () => {
+    modalApp.loadModal();
+}
 
-
-
-//GALLERY CAROUSEL
-
-//user clicks next button or previous button to navigate through a slide deck of images
-
+modalApp.init();
