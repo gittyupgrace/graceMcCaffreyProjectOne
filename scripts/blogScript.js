052846758b56eddf54ecc9e5a70fe0592fc71662
Grace <gracecmccaffrey@gmail.com>
2022-10-17 17:51:20 -0400

    refactored js and added namespacing for blog commenting feature

--- scripts/blogScript.js
+++ scripts/blogScript.js
@@ -1,76 +1,73 @@
-//BLOG COMMENTS
+const commentApp = {};
 
-//INITIAL PSEUDOCODE:
+commentApp.commentForm = document.querySelector('.commentForm');
+commentApp.userName = document.querySelector('input[type=text]');
+commentApp.userComment = document.querySelector('textarea');
+commentApp.commentSection = document.querySelector('.commentSection');
 
-//pull form, name, message from html
-//find a way to include date comment was made
-
-//use an event listener to capture and find value of name and message user inputs
-
-//display these values with hard coded comments (append a new div to .commentSection div)
-
-//Bonus stuff: 
-//fix date to match design brief
-//add bottom yellow border to every second comment
-//don't let user hit submit unless they have inputed info into all fields
-
-
-//PROCESS PSEUDOCODE:
-
-
-//Grab needed elements from page
-const commentForm = document.querySelector('.commentForm');
-const userName = document.querySelector('input[type=text]');
-const userComment = document.querySelector('textarea');
-const commentSection = document.querySelector('.commentSection');
-
-//Check to make sure they're all appearing:
-// console.log(commentForm);
-// console.log(userName);
-// console.log(userComment);
-// console.log(commentSection);
-
-//Add event listener to form to capture user input values when user hits submit and then display those inputs on the page using the css styling already in place
-
-commentForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    const nameInput = userName.value;
-    const commentInput = userComment.value;
-
-    //create new div with a class of "comment" for each new comment
-    const newComment = document.createElement('div');
-    newComment.classList.add('comment');
-
-    //created div for newComment, append this div on commentSection 
-    commentSection.append(newComment);
-
-    //for js to match original html/css, within newComment div, there needs to be two more divs, one for the user avatar img and one for the input values (ie the user name, comment and date they commented)
-
+commentApp.addUserAvatar = () => {
     const commentAvatar = document.createElement('div');
     commentAvatar.classList.add('commentAvatar');
     const avatarImg = document.createElement('img');
     avatarImg.src = "./assets/avatar-placeholder.png";
     commentAvatar.appendChild(avatarImg);
-    newComment.appendChild(commentAvatar);
+    commentApp.newComment.appendChild(commentAvatar);
+}
 
-    const commentContent = document.createElement('div');
-    commentContent.classList.add('commentContent');
-
+commentApp.addCommentDate = () => {
     const date = new Date();
     const postDate = date.toDateString();
     const nameA = document.createElement('a');
-    nameA.textContent = `${postDate} by ${nameInput}`;
-    commentContent.appendChild(nameA);
+    nameA.textContent = `${postDate} by ${commentApp.nameInput}`;
+    commentApp.commentContent.appendChild(nameA);
+}
 
+commentApp.addCommentContent = () => {
     const commentP = document.createElement('p');
     commentP.classList.add('commentContent');
-    commentP.textContent = `${commentInput}`;
-    commentContent.appendChild(commentP);
+    commentP.textContent = `${commentApp.commentInput}`;
+    commentApp.commentContent.appendChild(commentP);
+    commentApp.newComment.appendChild(commentApp.commentContent);
+}
 
-    newComment.appendChild(commentContent);
+//collect content for new comment
+commentApp.collectCommentContent = () => {
+    //grab user input values
+    commentApp.nameInput = commentApp.userName.value;
+    commentApp.commentInput = commentApp.userComment.value;
+    //create container for user inputs with relevant class name
+    commentApp.commentContent = document.createElement('div');
+    commentApp.commentContent.classList.add('commentContent');
+    //add user avatar to new comment div
+    commentApp.addUserAvatar();
+    //add current date
+    commentApp.addCommentDate();
+    //add comment content ie user inputs
+    commentApp.addCommentContent();
+}
 
+//create a place for new comment
+commentApp.displayNewComment = () => {
+    //create new div with a class of "comment" for each new comment
+    commentApp.newComment = document.createElement('div');
+    commentApp.newComment.classList.add('comment');
+    //add content to this new comment div
+    commentApp.collectCommentContent();
+    //append this new comment div onto commentSection 
+    commentApp.commentSection.append(commentApp.newComment);
+}
 
-    //reset form input values after sumbit so form is empty for next user
-    commentForm.reset();
-});
+//function that posts complete new comment when user hits submit
+commentApp.postNewComment = () => {
+    commentApp.commentForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        commentApp.displayNewComment();
+        commentApp.commentForm.reset();
+    });
+}
+
+commentApp.init = () => {
+    commentApp.postNewComment();
+}
+
+commentApp.init();
